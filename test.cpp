9336cd2b1f847d6767f7d38997d70b70039a1675
Sanjeev Devarajan <realsanjeevd@gmail.com>
2023-01-21 14:04:08 -0600

    asd

--- test.cpp
+++ test.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 
-int main() {
-    std::cout << 1 + 2;
+int main()
+{
+	std::cout << "Enter a number! ";
+
+    int num{ };
+    std::cin >> num;
+
+    std::cout << "Double that is " << num * 2 << "\n";
+    std::cout << "Triple that is " << num * 3 << "\n";
 
     return 0;
 }
